08d631ebf12372a0fed46cf590bde55d8672783d
Smit Butani <smit9081806441@gmail.com>
2026-09-12 11:08:29 +0530

    solv issue

--- src/components/GoogleAnalytics.jsx
+++ src/components/GoogleAnalytics.jsx
@@ -49,7 +49,7 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
           src="https://www.googletagmanager.com/ns.html?id=GTM-WHLQR9CS"
           height="0"
           width="0"
-          style="display:none;visibility:hidden"
+          style={{ display: "none", visibility: "hidden" }}
         ></iframe>
       </noscript>
     </>
